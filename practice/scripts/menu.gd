49e730fbe3b7c3ed866027e84d47608d55a94b7c
alexPrice2123 <alexander.price2123@gmail.com>
2025-03-24 21:13:56 -0600

    titlescreen

---
+++ practice/scripts/menu.gd
@@ -0,0 +1,52 @@
+extends Node2D
+@export var cursor = preload("res://Images/hand.png")
+var swamp = preload("res://Scenes/Swamp.tscn").instantiate()
+
+func _ready() -> void:
+	$Sprites/Skeli.play("default")
+	Input.set_custom_mouse_cursor(cursor, Input.CURSOR_POINTING_HAND, Vector2(0, 0))
+	while true:
+		position.x -= 2
+		await get_tree().create_timer(0.01).timeout
+
+
+func _on_play_button_down() -> void:
+	var pressedbutton = load("res://Images/HUD/Button/Pressed.png")
+	$HUD/Control/Play.icon = pressedbutton
+
+func _on_play_button_up() -> void:
+	var pressedbutton = load("res://Images/HUD/Button/NormalButton.png")
+	$HUD/Control/Play.icon = pressedbutton
+	$Sound.stream = load("res://Sounds/Sounds/Confirm.wav")
+	$Sound.play()
+	$HUD/Control/Overlay.visible = true
+	var times = 0.01
+	for i in 100:
+		$HUD/Control/Overlay.material.set_shader_parameter("progress", times)
+		times += 0.01
+		await get_tree().create_timer(0.01).timeout
+	get_tree().change_scene_to_file("res://Scenes/Swamp.tscn")
+func _on_quit_button_down() -> void:
+	var pressedbutton = load("res://Images/HUD/Button/Pressed.png")
+	$HUD/Control/Quit.icon = pressedbutton
+
+
+func _on_quit_button_up() -> void:
+	var pressedbutton = load("res://Images/HUD/Button/NormalButton.png")
+	$HUD/Control/Quit.icon = pressedbutton
+	$Sound.stream = load("res://Sounds/Sounds/Cancel.wav")
+	$Sound.play()
+	$HUD/Control/Overlay.visible = true
+	var times = 0.01
+	for i in 100:
+		$HUD/Control/Overlay.material.set_shader_parameter("progress", times)
+		times += 0.01
+		await get_tree().create_timer(0.01).timeout
+	get_tree().quit()
+
+func change_scene_to_node(node):
+	var tree = get_tree()
+	var cur_scene = tree.get_current_scene()
+	tree.get_root().add_child(node)
+	tree.get_root().remove_child(cur_scene)
+	tree.set_current_scene(node)
